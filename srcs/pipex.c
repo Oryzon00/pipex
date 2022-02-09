656746a864f0c25b35433471270ad7785cacf360
Adrian JUNG <ajung@e1r8p1.42.fr>
2022-02-09 20:32:29 +0100

    push auto

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 19:43:31 by ajung             #+#    #+#             */
-/*   Updated: 2022/02/09 19:19:13 by ajung            ###   ########.fr       */
+/*   Updated: 2022/02/09 20:11:08 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,28 @@
 
 int	left_side(int infile, int *pont, t_arg_main *arg_main)
 {
-	//cmd1 = argv[2]
 	if (dup2(infile, STDIN_FILENO) < 0)
 		return (EXIT_FAILURE);	
 	if (dup2(pont[1], STDOUT_FILENO) < 0)
 		return (EXIT_FAILURE);
 	close(pont[0]); //on ferme autre cote du pont;
 	close(infile); //il faut fermer ce qu'on ouvre
-
-	//execve des paths
-
+	if (exec_cmd(arg_main, 2) == EXIT_FAILURE)
+		return (EXIT_FAILURE); 	//cmd1 = argv[2]
 	//Si on arrive la, c'est que execve n'a pas marche et donc c'est un failure;
 	return(EXIT_FAILURE);
 }
 
 int	right_side(int outfile, int *pont, t_arg_main *arg_main)
 {
-	//cmd2 = argv[3]
-	int	status;
-
 	if (dup2(outfile, STDOUT_FILENO) < 0)
 		return (EXIT_FAILURE);
 	if (dup2(pont[0], STDIN_FILENO) < 0)
 		return (EXIT_FAILURE);
 	close(pont[1]); //on ferme autre cote du pont
 	close(outfile);
-	
-	//execve fuction for paths
-
+	if (exec_cmd(arg_main, 3) == EXIT_FAILURE)  //cmd2 = argv[3]
+		return (EXIT_FAILURE);
 	//Si on arrive la c'est que execve a pas bien marche;
 	return (EXIT_FAILURE);
 }
@@ -58,16 +52,16 @@ void	pipex(int infile, int outfile, t_arg_main *arg_main)
 	if (child_left < 0)
 		return (perror("Fork: "));
 	if (child_left == 0)
-		left_side(infile, pont, arg_main);
-		//if LEFT_SIde == EXIT FAILURE --> lancer clean + fermeture programme	}
+		if (left_side(infile, pont, arg_main) == EXIT_FAILURE)
+			perror_and_exit();
 	waitpid(child_left, &status, 0); //on attends que left soit fini pour recuperer
 									// l'output dans pont[0]
 	child_right = fork();
 	if (child_right < 0)
 		return (perror("Fork: "));
 	if (child_right == 0)
-		right_side(outfile, pont, arg_main);
-		//if RIGHT_SIDE == EXIT FAILURE --> lancer clean + fermeture programme
+		if (right_side(outfile, pont, arg_main) == EXIT_FAILURE)
+			perror_and_exit();
 	close(pont[0]); //on ferme le pont --> parent ne fait rien
 	close(pont[1]);
 	waitpid(child_left, &status, 0); //on attends que les enfants est fini leurs taches
--- srcs/pipex_utils.c
+++ srcs/pipex_utils.c
@@ -6,14 +6,14 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 21:11:11 by ajung             #+#    #+#             */
-/*   Updated: 2022/02/09 19:13:58 by ajung            ###   ########.fr       */
+/*   Updated: 2022/02/09 20:27:39 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
 
-char	**get_cmd_path(char **envp, char **argv)
+char	**get_cmd_path(char **envp)
 {
 char	*PATH_envp;
 char	**cmd_paths;
@@ -25,7 +25,7 @@ while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
 PATH_envp = ft_strdup(envp[i]);
 if (PATH_envp == NULL)
 	return (NULL);
-cmd_paths = ft_split(PATH_envp + 5, ":");
+cmd_paths = ft_split(PATH_envp + 5, ':');
 if (cmd_paths == NULL)
 	return (NULL);
 free(PATH_envp);
@@ -38,38 +38,50 @@ char	**get_cmd_arg(char **argv, int cmd_num) //cmd_num = 2 ou 3
 {
 	char **cmd_args;
 
-	cmd_args = ft_split(argv[cmd_num], " ");
+	cmd_args = ft_split(argv[cmd_num], ' ');
 	return (cmd_args);
 }
 
+char	*get_cmd_no_arg(t_arg_main *arg_main, int cmd_num)
+{
+	char	**ret_split;
+	char	*cmd_no_arg;
+
+	ret_split = ft_split(arg_main->argv[cmd_num], ' ');
+	cmd_no_arg = ft_strdup(ret_split[0]);
+	free_split(ret_split);
+	return (cmd_no_arg);
+}
 
 int exec_cmd(t_arg_main *arg_main, int cmd_num)  //cmd_num = 2 ou 3
 {
-int  i;
-char *cmd;
-char **cmd_paths;
-char **cmd_args;
+int		i;
+char	*cmd;
+char	*cmd_no_arg;
+char 	**cmd_paths;
+char 	**cmd_args;
 
 cmd_args = get_cmd_arg(arg_main->argv, cmd_num); //peut etre sur NULL si pas argument
-cmd_paths = get_cmd_path(arg_main->envp, arg_main->argv);
+cmd_paths = get_cmd_path(arg_main->envp);
+cmd_no_arg = get_cmd_no_arg(arg_main, cmd_num);
 if (cmd_paths == NULL)
 	return (EXIT_FAILURE);
 i = 0;
 while (cmd_paths[i]) 
 {
-    cmd = ft_strjoin(cmd_paths[i], arg_main->argv[cmd_num]);
+    cmd = ft_strjoin(cmd_paths[i], cmd_no_arg);
 	if (!cmd)
 	{
-		free_path_and_args(cmd_paths, cmd_args);
+		free_path_and_args(cmd_paths, cmd_args, cmd_no_arg);
 		return (EXIT_FAILURE);
 	}
 	//execve devrait avance tout seul de 1 si cmd_arg[0] est la commande
+	printf("cmd = %s, \ncmd_arg[1] = %s\n, cmd_no_arg = %s\n\n", cmd, cmd_args[1], cmd_no_arg);
     execve(cmd, cmd_args, arg_main->envp); // if execve succeeds, it exits and free
-    //perror("Error"); //<- add perror to debug
     free(cmd); // if execve fails, we free and we try a new path
 	i++;
 }
 //si on a rien trouve on free tout
-free_path_and_args(cmd_paths, cmd_args);
+free_path_and_args(cmd_paths, cmd_args, cmd_no_arg);
 return (EXIT_FAILURE);
 }
