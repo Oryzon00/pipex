5dc5724faadb7153934e683e938e2dff39c173f1
Adrian JUNG <ajung@e2r10p9.clusters.42paris.fr>
2022-02-10 19:00:20 +0100

    push auto

--- srcs/check_error.c
+++ srcs/check_error.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 17:28:33 by ajung             #+#    #+#             */
-/*   Updated: 2022/02/09 18:57:21 by ajung            ###   ########.fr       */
+/*   Updated: 2022/02/10 15:04:10 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	check_error(int argc, char **argv)
 {
 	if (argc != 5)
 	{
-		ft_putstr_fd("Error: nombre d'argument != 4", 2);
+		ft_putstr_fd("Error: nombre d'argument != 4\n", 2);
 		exit (0);
 	}
 	(void) argv;
--- srcs/pipex_utils2.c
+++ srcs/pipex_utils2.c
@@ -6,7 +6,7 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 18:44:52 by ajung             #+#    #+#             */
-/*   Updated: 2022/02/09 20:26:23 by ajung            ###   ########.fr       */
+/*   Updated: 2022/02/10 18:28:49 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	perror_and_exit(void)
 {
-	perror("Error");
-	exit (-1);
+	perror("pipex");
+	exit (127);
 }
 
 void	fill_arg_main(t_arg_main *arg_main, int argc, char **argv, char **envp)
